a0d855afeaff5620116be48a725450a106ddde05
S.Ignatyuk <signatyuk89@gmail.com>
2021-03-05 16:04:01 +0300

    employees.html v2.0

--- src/main/java/TirService/controller/EmployeeController.java
+++ src/main/java/TirService/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import TirService.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,9 +19,9 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping
     public String getAllEmployees(ModelMap modelMap) {
         modelMap.addAttribute("employees", employeeService.getAllEmployees());
-        return "employees.html";
+        return "/employees";
     }
 }
